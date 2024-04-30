6ca9deee306daf7b643061bef0e8b3bc3d8ab3f4
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-04-30 13:49:56 +0530

    Gmail MERn

---
+++ index.js
@@ -0,0 +1,147 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const { google } = require('googleapis');
+const gmail = google.gmail('v1');
+
+dotenv.config();
+
+const app = express()
+const PORT=process.env.PORT;
+
+const MONGO_URL= process.env.MONGO_URL;
+
+async function createConnection() {
+  try {
+    mongoose.connect(MONGO_URL)
+    console.log("Connected to Database Successfully ")
+} catch (error) {
+    console.log(`Error connecting to Mongo db pls Check: ${error} `);
+    throw error
+}
+}
+
+const client = createConnection();
+
+
+app.use(cors({
+  origin:'*', // allow to run on any origins
+}))
+app.use(express.json());
+
+app.get('/', function (request, response) {
+  response.send("hello this a home page of all data")
+});
+
+app.get('/gmail_data', async function (request, response) {
+  const movies = await client.db("gmail").collection("primary").find({}).toArray();
+ response.send(movies)
+});
+
+app.get("/gmail_data/:name", async function (request, response) {
+  const { name }=request.params;
+  const result = await client.db("gmail").collection("primary").find({name:name}).toArray();
+ response.send(result)
+});
+
+app.put("/gmail_data/:id", async function (request, response) {
+
+  const { id }=request.params;
+  console.log(request.params, id);
+  const data= request.body;
+
+  const result=await client
+  .db("gmail")
+  .collection("primary")
+  .updateOne({ id : id },{$set:data});
+
+  result.modifiedCount > 0
+  ?response.send({msg:"Gmail successfully updated"})
+  :response.status(400).send({msg:"gmail not found"});
+});
+
+app.delete("/gmail_data/:name", async function (request, response) {
+  const { name }=request.params;
+  console.log(request.params, name);
+
+  const result=await client
+  .db("gmail")
+  .collection("primary")
+  .deleteOne({name : name });
+
+  result.deletedCount > 0
+  ?response.send({msg:"gmail successfully deleted"})
+  :response.status(401).send({msg:"gmail not found"});
+});
+
+app.post('/gmail_data', async function (request, response) {
+  const data = request.body;
+  const result = await client.db("gmail").collection("primary").insertOne(data);
+  response.send(result);
+ });
+
+//for sign up processs
+app.get('/users/signup', async function (request, response) {
+  const data = await client.db("gmail").collection("usersignup").find({}).toArray();
+ response.send(data)
+});
+
+//signup processs
+app.post('/users/signup', async function (request, response) {
+  const {username, password} = request.body; 
+  const userFromdb=await client
+  .db("gmail")
+  .collection("usersignup")
+  .findOne({username:username});
+  console.log(userFromdb);
+  if(userFromdb){
+    response.status(400).send({msg:'USER ALREADY EXISTS'})
+  }
+  else if(password.length<8){
+    response.status(402).send({msg:"password must be longer"});
+  }
+  else{
+    const hashedPassword=await genHashedPassword(password);
+    console.log(password, hashedPassword);
+    const result=await client.db("gmail").collection("usersignup").insertOne({username:username, password:hashedPassword});
+    response.send(result);
+  }
+ });
+
+ //login process
+app.post('/users/login', async function (request, response) {
+  const {username, password} = request.body; 
+  const userFromdb=await client
+  .db("gmail")
+  .collection("usersignup")
+  .findOne({username:username});
+  console.log(userFromdb);
+
+  if(!userFromdb){
+    response.status(401).send({msg:'invalid credentials'});
+  }else{
+    const storePassword=userFromdb.password;
+    const isPasswordMatch= await bcrypt.compare(password,storePassword);
+    console.log(isPasswordMatch);
+    
+    if(isPasswordMatch){
+      const token=jwt.sign({id:userFromdb._id},process.env.SECRET_KEY);
+      response.send({msg:"login successful",token:token});
+    }else{
+      response.status(401).send({msg:"Invalid Credentials"});
+    }
+  }
+ });
+
+app.listen(PORT,()=>console.log(`APP is running ${PORT}`))
+
+async function genHashedPassword(password){
+  const NO_OF_ROUNDS=10;
+  const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password,salt);
+  return hashedPassword
+}
